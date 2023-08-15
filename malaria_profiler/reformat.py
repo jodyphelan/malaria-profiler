65be0058f2b953fdacd35046b85c516da652b712
Jody Phelan <jody.phelan@lshtm.ac.uk>
2023-08-15 21:21:57 +0100

    improvements to amplicon

--- malaria_profiler/reformat.py
+++ malaria_profiler/reformat.py
@@ -1,4 +1,4 @@
-from pathogenprofiler import dict_list_add_genes, reformat_annotations, reformat_missing_genome_pos, select_csq
+from pathogenprofiler import load_bed, dict_list_add_genes, reformat_annotations, reformat_missing_genome_pos, select_csq
 
 def reformat(results,conf):
     results["variants"] = [x for x in results["variants"] if len(x["consequences"])>0]
@@ -7,7 +7,11 @@ def reformat(results,conf):
     results = reformat_annotations(results,conf)
 
     if "region_qc" in results["qc"]:
-        results["qc"]["region_qc"] = dict_list_add_genes(results["qc"]["region_qc"],conf,key="region")
+        if conf['amplicon']==True:
+            amplicon2gene = load_bed(conf['bed'],columns=[4,5,7],key1=7)
+            for d in results["qc"]["region_qc"]:
+                d["gene_id"] = amplicon2gene[d["region"]][0]
+        results["qc"]["region_qc"] = dict_list_add_genes(results["qc"]["region_qc"],conf)
     if "missing_positions" in results["qc"]:
         results["qc"]["missing_positions"] = reformat_missing_genome_pos(results["qc"]["missing_positions"],conf)
     
