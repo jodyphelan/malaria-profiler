8e761fdf407017cdb37cddc745049537033fd0e1
Jody Phelan <jody.phelan@lshtm.ac.uk>
2024-09-05 11:10:18 +0200

    changes

--- malaria_profiler/__init__.py
+++ malaria_profiler/__init__.py
@@ -1,8 +1,6 @@
 from .utils import *
 from .reformat import *
 from .output import *
-# from .profiler import *
 from .speciation import *
-from .get_haplotype import *
 from .geo_classifier import *
 __version__="0.0.8"
