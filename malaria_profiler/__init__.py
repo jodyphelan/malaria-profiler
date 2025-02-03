532c7b21bf156bdde3de2627107d053d12ee8c5f
Jody Phelan <jody.phelan@lshtm.ac.uk>
2025-02-03 15:00:35 +0000

    bump version

--- malaria_profiler/__init__.py
+++ malaria_profiler/__init__.py
@@ -5,7 +5,7 @@ from .speciation import *
 from .geo_classifier import *
 from .moi import *
 from pathogenprofiler.variant_calling import VariantCaller
-__version__="0.0.8"
+__version__="0.0.9"
 
 
 
