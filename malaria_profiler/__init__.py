d2210cfaaaea12a1dbaf9611b4b1ca2dccd799c1
Joe Thorpe <100434056+klausyboi@users.noreply.github.com>
2025-02-17 15:13:03 +0000

    Update __init__.py

--- malaria_profiler/__init__.py
+++ malaria_profiler/__init__.py
@@ -3,24 +3,4 @@ from .reformat import *
 from .output import *
 from .speciation import *
 from .geo_classifier import *
-from .moi import *
-from pathogenprofiler.variant_calling import VariantCaller
-__version__="0.0.9"
-
-
-
-
-class MalariaFreebayesCaller(VariantCaller):
-    __software__ = "freebayes-illumina-malaria"
-    def call_variants(self) -> Vcf:
-        # Call variants using Lofreq
-        
-        self.calling_cmd = """
-            samtools view -T %(ref_file)s  -h %(bam_file)s {region} %(samclip_cmd)s | \
-            samtools view -b > %(temp_file_prefix)s.{region_safe}.tmp.bam && \
-            samtools index %(temp_file_prefix)s.{region_safe}.tmp.bam && \
-            freebayes -f %(ref_file)s -r {region} %(calling_params)s  %(temp_file_prefix)s.{region_safe}.tmp.bam | \
-            bcftools norm -a -Oz -o %(temp_file_prefix)s.{region_safe}.vcf.gz
-            """ % vars(self)
-        
-        return self.run_calling(self.calling_cmd)
+__version__="0.0.8"
